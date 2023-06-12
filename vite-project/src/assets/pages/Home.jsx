3a4903c4687ae2838b463d19cd4f967a08699396
edmundsamina <edmundsamina@gmail.com>
2023-06-12 16:38:32 +0100

    file error fixed

--- vite-project/src/assets/pages/Home.jsx
+++ vite-project/src/assets/pages/Home.jsx
@@ -1,15 +1,16 @@
-import { Landing } from '../components/landing/Landing';
-import { NewCarousel } from '../components/landing/new-carousel/NewCarousel';
-import { LandingBlog } from '../components/landing/blog/LandingBlog';
-import { How } from '../components/landing/how/how';
-export function Home(){
-    return (
-        <div className="home-container">
-        <Landing/>
-        <NewCarousel/>
-        <How/>
+import { Landing } from "../components/landing/Landing";
+import { NewCarousel } from "../components/landing/new-carousel/NewCarousel";
+import { LandingBlog } from "../components/landing/blog/LandingBlog";
+import { How } from "../components/landing/how/How";
 
-        <LandingBlog/>        
-        </div>
-    )
+export function Home() {
+  return (
+    <div className="home-container">
+      <Landing />
+      <NewCarousel />
+      <How />
+
+      <LandingBlog />
+    </div>
+  );
 }
