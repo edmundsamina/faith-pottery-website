19c16d461ce23cb5f2bf5f4df9ea7261ae80ee0c
edmundsamina <edmundsamina@gmail.com>
2023-06-13 14:20:55 +0100

    svgs downloaded, react router error fix attempt

--- vite-project/src/assets/pages/Root.jsx
+++ vite-project/src/assets/pages/Root.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/header-footer/Header";
 import { Footer } from "../components/header-footer/Footer";
+
 export function Root(){
     return (
         <div className="root-page-div">
